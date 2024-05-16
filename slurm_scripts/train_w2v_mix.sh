025ea35cf3f3af1199a85724085c876ca1736cbf
jacksukk <jacksukk@gmail.com>
2024-05-17 00:36:41 +0800

    update

--- slurm_scripts/train_w2v_mix.sh
+++ slurm_scripts/train_w2v_mix.sh
@@ -5,8 +5,7 @@
 #SBATCH --account=MST113025
 #SBATCH -o ./slurm_logs/slurm-%A_%a.out
 #SBATCH --ntasks-per-node=1
-#SBATCH --array=1-56
-#SBATCH --partition=gp4d
+#SBATCH --array=1-14
 
 module purge
 module load miniconda3
@@ -16,14 +15,14 @@ export TRANSFORMERS_CACHE=/tmp
 export HF_DATASETS_CACHE=/tmp
 export HUGGINGFACE_HUB_CACHE=/tmp
 export TRANSFORMERS_CACHE=/tmp
-
 domain=('cooking' 'audio' 'transport' 'news' 'music' 'lists' 'weather' 'calendar' 'qa' 'general' 'datetime' 'recommendation' 'play' 'iot' 'social' 'takeaway' 'email' 'alarm')
 multi_target_domain=('audio' 'transport' 'news' 'lists' 'calendar' 'qa' 'general' 'datetime' 'recommendation' 'play' 'iot' 'takeaway' 'email' 'alarm')
-
 final_train_domain=()
 syn=()
-for target_domain in "${multi_target_domain[@]}"; do
+for target_domain in "${temp_domain[@]}"; do
+    
     train_domain=""
+
     for d in "${domain[@]}"; do
         if [ $d == "$target_domain" ]; then
             continue
@@ -31,19 +30,13 @@ for target_domain in "${multi_target_domain[@]}"; do
         train_domain+="$d;"
     done
 
-    echo $train_domain;
-    echo $target_domain;
-    final_train_domain+=("$train_domain" "$target_domain" "$train_domain" "$target_domain");
+    # echo $train_domain;
+    # final_train_domain=("$train_domain" "$target_domain" "$train_domain" "$target_domain");
+    final_train_domain+=("$train_domain");
 
-    # final_train_domain=("$target_domain" "$target_domain" "$target_domain");
-
-    syn+=("False" "False" "True" "True");
+    syn+=("True");
+    echo ${final_train_domain[$SLURM_ARRAY_TASK_ID-1]};
 done
-# syn=("True" "True");
-
 echo ${final_train_domain[2]};
-# if [ $SLURM_ARRAY_TASK_ID == 14 ]; then
-#     echo $SLURM_ARRAY_TASK_ID;
-python train_w2v.py --domains "${final_train_domain[$SLURM_ARRAY_TASK_ID-1]}" --syn "${syn[$SLURM_ARRAY_TASK_ID-1]}" --model_path facebook/wav2vec2-conformer-rope-large-960h-ft;
-# fi
+python train_w2v.py --domains "${final_train_domain[$SLURM_ARRAY_TASK_ID-1]}" --syn "${syn[$SLURM_ARRAY_TASK_ID-1]}" --mix True --model_path facebook/wav2vec2-conformer-rope-large-960h-ft;
 # python train_origin.py;
