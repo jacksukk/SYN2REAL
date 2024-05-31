3b1f4053dedae8332dfce9c1d6506d65daf908db
jacksukk <jacksukk@gmail.com>
2024-05-31 11:38:23 +0800

    update

--- slurm_scripts/train_nums.sh
+++ slurm_scripts/train_nums.sh
@@ -18,7 +18,7 @@ export TRANSFORMERS_CACHE=/tmp
 
 domain=('cooking' 'audio' 'transport' 'news' 'music' 'lists' 'weather' 'calendar' 'qa' 'general' 'datetime' 'recommendation' 'play' 'iot' 'social' 'takeaway' 'email' 'alarm')
 multi_target_domain=("music")
-nums=(1 3 5 7 9 11 13 15)
+nums=(1 1 3 3 5 5 7 7 9 9 11 11 13 13 15 15)
 final_train_domain=()
 syn=()
 for target_domain in "${multi_target_domain[@]}"; do
@@ -43,9 +43,15 @@ done
 echo ${final_train_domain[2]};
 # torchrun --nproc_per_node 2 train.py;
 # sleep $(((SLURM_ARRAY_TASK_ID-1)*120));
-# if [ $SLURM_ARRAY_TASK_ID == 3 ] || [ $SLURM_ARRAY_TASK_ID == 5 ] || [ $SLURM_ARRAY_TASK_ID == 7 ]; then
-    # echo $SLURM_ARRAY_TASK_ID;
+if [ $SLURM_ARRAY_TASK_ID == 7 ]; then
+    echo $SLURM_ARRAY_TASK_ID;
 python train.py --domains "${final_train_domain[$SLURM_ARRAY_TASK_ID-1]}" --syn "${syn[$SLURM_ARRAY_TASK_ID-1]}" --model_path openai/whisper-small --configs configs/whisper_small.yaml --numbers ${nums[$SLURM_ARRAY_TASK_ID-1]};
-# fi
+fi
 # python train.py --domains "${final_train_domain[$SLURM_ARRAY_TASK_ID-1]}" --syn "${syn[$SLURM_ARRAY_TASK_ID-1]}" --model_path openai/whisper-tiny --configs configs/whisper_tiny.yaml;
 # python train_origin.py;
+
+
+#  if [ $SLURM_ARRAY_TASK_ID == 15 ] || [ $SLURM_ARRAY_TASK_ID == 16 ] || [ $SLURM_ARRAY_TASK_ID == 17 ] || [ $SLURM_ARRAY_TASK_ID == 18 ] || [ $SLURM_ARRAY_TASK_ID == 19 ] ; then
+
+# python train.py --domains "${final_train_domain[$SLURM_ARRAY_TASK_ID-1]}" --syn "${syn[$SLURM_ARRAY_TASK_ID-1]}" --model_path openai/whisper-base --configs configs/whisper_base.yaml;
+# fi
