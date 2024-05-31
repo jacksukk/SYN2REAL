81d5337dc7c8eadf1cf8365a630152c9c24eec33
jacksukk <jacksukk@gmail.com>
2024-06-01 00:54:16 +0800

    update

--- slurm_scripts/test_vector.sh
+++ slurm_scripts/test_vector.sh
@@ -17,10 +17,11 @@ export TRANSFORMERS_CACHE=/tmp
 
 domain=('cooking' 'audio' 'transport' 'news' 'music' 'lists' 'weather' 'calendar' 'qa' 'general' 'datetime' 'recommendation' 'play' 'iot' 'social' 'takeaway' 'email' 'alarm')
 # temp_domain=('cooking' 'audio' 'transport' 'news' 'music' 'lists' 'weather' 'calendar' 'qa' 'general' 'datetime' 'recommendation' 'play' 'iot' 'social' 'takeaway' 'email' 'alarm')
-temp_domain=('music' 'cooking' 'weather' 'social')
+temp_domain=('music')
+nums=(1 3 5 7 9 11 13 15)
 # model=("openai/whisper-large" "outputs/whisper_slurp_"$target_domain"_large" "outputs/whisper_slurp_"$target_domain"_synthetic_large" "outputs/whisper_slurp_"$target_domain"_anti_large" "outputs/whisper_slurp_"$target_domain"_anti_synthetic_large" "outputs/whisper_slurp_"$target_domain"_anti_mixed_large/")
 
 echo ${temp_domain[$SLURM_ARRAY_TASK_ID-1]};
 sleep $(((SLURM_ARRAY_TASK_ID-1)*60));
 
-python test.py --model_syn_anti outputs/whisper_slurp_dpwd__${temp_domain[$SLURM_ARRAY_TASK_ID-1]}_anti_synthetic_speech_t5_small/ --model_anti outputs/whisper_slurp_dpwd__${temp_domain[$SLURM_ARRAY_TASK_ID-1]}_anti_small/ --model_target_syn outputs/whisper_slurp_dpwd__${temp_domain[$SLURM_ARRAY_TASK_ID-1]}_anti_mixed_speech_t5_small_continue/ --domain ${temp_domain[$SLURM_ARRAY_TASK_ID-1]} --weight 0.25
+python test.py --model_syn_anti outputs/whisper_slurp_${temp_domain[$SLURM_ARRAY_TASK_ID-1]}_anti${nums[$SLURM_ARRAY_TASK_ID-1]}_synthetic_small/ --model_anti outputs/whisper_slurp_${temp_domain[$SLURM_ARRAY_TASK_ID-1]}_anti${nums[$SLURM_ARRAY_TASK_ID-1]}_small/ --model_target_syn "jacksukk/whisper_slurp_music_anti_mixed_small_continue" --domain ${temp_domain[$SLURM_ARRAY_TASK_ID-1]} --weight 0.2
